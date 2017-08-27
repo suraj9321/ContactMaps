8d0a5e472b859909630777e549c85a050fc25b46
suraj9321 <suraj9321@gmail.com>
2017-08-28 00:36:22 +0530

    This app shows the dowloaded contact to save and show location in google map

---
+++ app/src/main/java/com/example/surajsah/contactmapsave/Contact.java
@@ -0,0 +1,14 @@
+package com.example.surajsah.contactmapsave;
+
+
+
+public class Contact {
+    String name;
+    String email;
+    String location;
+    public Contact(String name,String email,String location){
+        this.name=name;
+        this.email=email;
+        this.location=location;
+    }
+}
